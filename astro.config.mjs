8141332c2656cbdeb7e8a699af201c41323622d3
MaxxusX <66540504+MaxxusX@users.noreply.github.com>
2024-08-28 01:59:34 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -33,11 +33,16 @@ export default defineConfig({
 		css: {
 			transformer: "lightningcss",
 			lightningcss: {
+				minify: true,
+				sourceMap: import.meta.env.DEV ? true : false,
+				errorRecovery: false,
+				/* smh gotta install browserslist
 				targets: browserslistToTargets(
 					browserslist(
 						">0.2% or last 3 versions or Firefox ESR and fully supports es6-module and fully supports es6-module-dynamic-import and not dead and not op_mini all and not and_uc 15 and not and_qq 14 and not kaios 3"
 					)
 				),
+    				*/
 			},
 		},
 		build: {
