c5601d18171e2d5d75a00af84f6a9caaf96b8d8b
MaxxusX <CarterWinden@gmail.com>
2024-08-29 03:00:22 -0500

    yay

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig, passthroughImageService } from "astro/config";
-import { browserslistToTargets } from "lightningcss";
 
-// https://astro.build/config
+const isProd = import.meta.env.PROD;
+
 export default defineConfig({
 	site: "https://maxxusx.github.io/nullhub-site/",
 	base: "/nullhub-site/",
 	trailingSlash: "ignore",
 	output: "static",
-	compressHTML: true,
+	compressHTML: isProd,
 	build: {
 		format: "file",
 		inlineStylesheets: "never",
@@ -27,26 +27,39 @@ export default defineConfig({
 				limitInputPixels: false,
 			},
 		},
-			*/
+		*/
 	},
 	vite: {
 		css: {
 			transformer: "lightningcss",
 			lightningcss: {
-				minify: true,
-				sourceMap: import.meta.env.DEV ? true : false,
+				minify: isProd,
+				sourceMap: false,
 				errorRecovery: false,
-				/* smh gotta install browserslist
-				targets: browserslistToTargets(
-					browserslist(
-						">0.2% or last 3 versions or Firefox ESR and fully supports es6-module and fully supports es6-module-dynamic-import and not dead and not op_mini all and not and_uc 15 and not and_qq 14 and not kaios 3"
-					)
-				),
-    				*/
+				targets: {
+					and_chr: 127 << 16,
+					and_ff: 127 << 16,
+					android: 127 << 16,
+					chrome: 109 << 16, // vite default: 87
+					edge: 120 << 16, // vite default: 88
+					firefox: 115 << 16, // vite default: 78
+					ios_saf: (15 << 16) | (6 << 8),
+					op_mob: 80 << 16,
+					opera: 109 << 16,
+					safari: (15 << 16) | (6 << 8), // vite default: 14
+					samsung: 25 << 16,
+				},
 			},
 		},
 		build: {
-			cssMinify: "lightningcss",
+			// vite default: ["es2020", "edge88", "firefox78", "chrome87", "safari14"]
+			target: ["es2020", "edge120", "firefox115", "chrome109", "safari15.6"],
+			assetsInlineLimit: 0,
+			cssCodeSplit: false,
+			cssMinify: isProd ? "lightningcss" : false,
+			minify: isProd ? "esbuild" : false,
+			sourcemap: false,
+			reportCompressedSize: false,
 		},
 	},
 });
